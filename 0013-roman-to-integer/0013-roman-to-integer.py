e716d62073eed2d307a394d75514264d321aef69
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-11 03:21:24 +0530

    Time: 11 ms (15.54%), Space: 17.9 MB (21.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,49 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        num = list(s)
-
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
         total = 0
-        prev = None 
 
-        for i in range(len(num)):
-            if num[i] == 'M':
-                if (prev == 'C'):
-                    total += 800
-                else:
-                    total += 1000
-                prev = "M"
-            elif num[i] == 'D':
-                if (prev== 'C'):
-                    total += 300
-                else:
-                    total += 500
-                prev = 'D'
-            elif num[i] == 'C':
-                if (prev== 'X'):
-                    total += 80
-                else:
-                    total += 100
-                prev = 'C'
-            elif num[i] == 'L':
-                if (prev== 'X'):
-                    total += 30
-                else:
-                    total += 50
-                prev = 'L'
-            elif num[i] == 'X':
-                if (prev== 'I'):
-                    total += 8
-                else:
-                    total += 10
-                prev = 'X'
-            elif num[i] == 'V':
-                if (prev== 'I'):
-                    total += 3
-                else:
-                    total += 5
-                prev = 'V'
-            elif num[i] == 'I':
-                total += 1
-                prev = 'I'
-
+        for i in range (len(s)):
+            if i < len(s) -1 and roman [s[i]] < roman[s[i+1]]:
+                total -= roman [s[i]] 
+            else:
+                total +=  roman[s[i]]
         return total
