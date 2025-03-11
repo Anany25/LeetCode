f38d3e4895904ca6306a65910a08c70fff809092
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-11 13:59:04 +0530

    Time: 3 ms (44.98%), Space: 17.9 MB (52.39%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,35 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x = list(a)
+        y = list(b)
+        if len(x) < len(y):
+            while len(x) != len(y):
+                x.insert(0,0)
+        elif len(x) > len(y):
+            while len(x) != len(y):
+                y.insert(0,0)
+        ans = []
+        carry = 0
+        i = len(x)-1
+        while i >= 0:
+            z = int(x[i]) + int(y[i]) + carry
+            if z == 1:
+                ans.insert(0,1)
+                carry = 0
+            elif z == 2:
+                ans.insert(0,0)
+                carry = 1
+            elif z == 3:
+                ans.insert(0,1)
+                carry = 1
+            elif z == 0:
+                ans.insert(0,0)
+                carry = 0
+            i -= 1
+        if carry == 1:
+            ans.insert(0,1)
+
+        final =0
+        for i in ans:
+            final = final* 10 + i
+        return str(final)
