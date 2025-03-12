c85356e62bad5401dbdb86c9a4187707572ba458
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-13 02:10:40 +0530

    Time: 5067 ms (11.11%), Space: 18.1 MB (29.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,9 +4,9 @@ class Solution:
         if len(s) <= 1:
             return s
 
-        maxi = 0
-        lp = s[0
-        ]
+        maxi = 1
+        lp = s[0]
+
         for i in range (0, len(s)-1):
             for j in range (i, len(s)):
                 if j-i+1 > maxi and s[i:j+1] == s[i:j+1][::-1]:
