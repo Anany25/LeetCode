986c549c92c07153f950eb71fd3a59dc361a28c8
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-13 13:39:21 +0530

    Time: 197 ms (70.11%), Space: 63.2 MB (68.09%) - LeetHub

---
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        
+        n = len(nums)
+        cum = [0] * (n + 1)
+
+
+        zero = True
+        for i in nums:
+            if i > 0:
+                zero = False
+
+        if zero is True:
+            return 0
+
+        for x in queries:
+            cum[x[0]] += x[2]
+
+            if x[1] + 1 < n:
+                cum[x[1] + 1] -= x[2]
+
+        cur = 0
+
+        for i in range(n):
+            cur += cum[i]
+            if cur < nums[i]:
+                return -1
+        
+        cum = [0] * (n + 1)
+
+        for k, x in enumerate(queries):
+            cum[x[0]] += x[2]
+
+            if x[1] + 1 < n:
+                cum[x[1] + 1] -= x[2]
+
+            cur = 0
+            flag = True
+            for i in range(n):
+                cur += cum[i]
+                if cur < nums[i]:
+                    flag = False
+                    break
+            
+            if flag:
+                return k+1
