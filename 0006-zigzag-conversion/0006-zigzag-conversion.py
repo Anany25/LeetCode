69431c7bef2d6bcc0cfeb0364adc875f3a3a0941
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-14 13:19:40 +0530

    Time: 8 ms (79.3%), Space: 18 MB (51.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,44 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        n = len (s)
+
         if numRows == 1 or numRows >= len(s):
             return s
 
-        pat = [[0] * n for _ in range(n)]
+        rows = [''] * numRows
+        curRow = 0
+        goingDown = False
 
-
-        pat[0][0] = s[0]
-        countdown, countup = 0, 0
-        i, j = 0, 0
-
-        for char in s[1:]:
-            if countdown < numRows-1:
-                i += 1
-
-                pat[i][j] = char
-                countdown +=1
-                if i > numRows:
-                    break
+        for char in s:
+            rows[curRow] += char
+    
+            if curRow == 0 or curRow == numRows - 1:
+                goingDown = not goingDown
                 
-            elif countdown == (numRows -1) and countup <numRows -1:
-                i -= 1
-                j += 1
-                if i > numRows:
-                    break
-                pat[i][j] = char
-                countup +=1
+            if goingDown:
+                curRow += 1 
+            else:
+                curRow -= 1
 
-            if countdown == (numRows -1) and countup == (numRows -1):
-                countdown, countup = 0, 0
-
-
-        ans = ""
-
-        f = [i for s in pat for i in s]
-        f = [i for i in f if i != 0]
-        for i in f:
-            if i != 0:
-                ans = ans + i
-
-        
-        return ans
+        return ''.join(rows)
