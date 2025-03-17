1248577e420643dd3ec3cca7f5c6b28a3e1fbeb7
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-17 05:48:13 +0530

    Time: 1043 ms (31.71%), Space: 22 MB (49.43%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -4,7 +4,7 @@ class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
         left = 0
         right = min(ranks) * cars * cars
-        mini = right
+
 
         while left <= right:
             time = (left + right)//2
@@ -14,12 +14,11 @@ class Solution:
                 repaired +=  int((time / rank) ** 0.5)
 
             if repaired >= cars:
-                mini = min(mini, time)
                 right = time - 1
             else:
                 left = time + 1
 
-        return mini
+        return left
 
 
 
