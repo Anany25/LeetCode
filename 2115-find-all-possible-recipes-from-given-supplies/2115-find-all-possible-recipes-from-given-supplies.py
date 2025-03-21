8147adb75e9350e8102e0545d59263dd04502669
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-21 09:46:24 +0530

    Time: 566 ms (95.44%), Space: 19.7 MB (58.7%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        ans = set()
+        comb = set()
+        comb.update(supplies)
+        n = len(recipes)
+        i = 0
+        while i < n:
+            temp = ingredients[i]
+            present = True
+            for ing in temp:
+                if ing not in comb:
+                    present = False
+                
+            if present is True and recipes[i] not in ans:
+                ans.add(recipes[i])
+                comb.add(recipes[i])
+                i = 0
+            else:
+                i += 1
+        
+
+        return list(ans)
