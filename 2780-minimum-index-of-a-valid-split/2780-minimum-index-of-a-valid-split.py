5dd8bb9adcf4cbad8c1e7eb9efe8830fd4362c27
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-27 06:17:36 +0530

    Time: 81 ms (48.52%), Space: 34.4 MB (50.99%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -8,13 +8,10 @@ class Solution:
         
         count = 0
         n = len(nums)
-        print(maxi)
 
         for i in range(n):
             if nums[i] == maxi:
                 count += 1
-
-            print(count, (i + 1) / 2, s[maxi] - count,(n - i - 1)/ 2)
             if count > (i + 1) / 2 and s[maxi] - count > (n - i - 1)/ 2:
                 return i
 
