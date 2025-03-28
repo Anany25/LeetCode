5996dc3426ab443bf074416bee91d901c1e2ecf3
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-28 19:29:52 +0530

    Time: 2556 ms (10.01%), Space: 18.2 MB (19.57%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        d1 , d2 = defaultdict(int), defaultdict(int)
+
+        for char in s1:
+            d1[char] += 1
+
+        n = len(s1)
+        m = len(s2)
+
+        left = 0
+
+        right = n
+
+        while right <= m:
+            
+            for char in range(left,right):
+                d2[s2[char]] += 1
+            print(d2)
+            if d1== d2:
+                return True
+
+            else:
+                d2.clear()
+                left += 1
+                right += 1
+
+        return False
+            
