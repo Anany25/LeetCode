3547dc75663582bd779e16f10bc4359976b61ed4
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-03-28 19:33:37 +0530

    Time: 15 ms (85.8%), Space: 17.7 MB (87.81%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,27 @@
+from collections import defaultdict
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        d1 , d2 = defaultdict(int), defaultdict(int)
+        if len(s1) > len(s2):
+            return False
+
+        d1, d2 = defaultdict(int), defaultdict(int)
 
         for char in s1:
             d1[char] += 1
 
-        n = len(s1)
-        m = len(s2)
-
         left = 0
+        for right in range(len(s2)):
+            d2[s2[right]] += 1
 
-        right = n
+            if right - left + 1 > len(s1):
+                if d2[s2[left]] == 1:
+                    del d2[s2[left]]
+                else:
+                    d2[s2[left]] -= 1
+                left += 1
 
-        while right <= m:
-            
-            for char in range(left,right):
-                d2[s2[char]] += 1
-    
-            if d1== d2:
+            if d1 == d2:
                 return True
 
-            else:
-                d2.clear()
-                left += 1
-                right += 1
-
         return False
-            
