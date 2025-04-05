3655f3f4b708a7fd98817e325b4c0c081b3448a5
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-04-06 02:22:35 +0530

    Time: 11 ms (76.64%), Space: 17.7 MB (82.84%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def dfs(i, xor_sum):
+            if i == len(nums):
+                return xor_sum
+            return dfs(i + 1, xor_sum ^ nums[i]) + dfs(i + 1, xor_sum)
+        return dfs(0, 0)
