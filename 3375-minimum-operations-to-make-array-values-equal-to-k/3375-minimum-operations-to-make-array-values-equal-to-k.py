da816eecea1392ba3d9904afe98060701769fdb7
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-04-09 23:57:57 +0530

    Time: 67 ms (54.25%), Space: 17.8 MB (35.61%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.py
@@ -3,13 +3,8 @@ class Solution:
 
         if min(nums) < k:
             return -1
-        nums.sort(reverse= True)
-        count = 0
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                count += 1
-
-        if nums[-1] > k:
-            count += 1
-        return count
+        s = set()
+        for num in nums:
+            if num > k and num not in s:
+                s.add(num)
+        return len(s)
