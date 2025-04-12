313bdd2d762d7c3b4dcc38cacd1632e8da3d6cf9
Anany25 <87889146+Anany25@users.noreply.github.com>
2025-04-12 09:43:21 +0530

    Time: 1479 ms (27.42%), Space: 20.5 MB (24.19%) - LeetHub

--- 3272-find-the-count-of-good-integers/3272-find-the-count-of-good-integers.py
+++ 3272-find-the-count-of-good-integers/3272-find-the-count-of-good-integers.py
@@ -1,10 +1,17 @@
+from math import factorial
+from collections import Counter
+
 class Solution:
     def countGoodIntegers(self, n: int, k: int) -> int:
+        
+        fact = [1] * (n + 1)
+        for i in range(2, n + 1):
+            fact[i] = fact[i - 1] * i
         
         def gen_palindrome(n):
             half = (n + 1) // 2
             start = 10 ** (half - 1)
-            end = 10 **half
+            end = 10 ** half
 
             for first_half in range(start, end):
                 s = str(first_half)
@@ -12,17 +19,17 @@ class Solution:
                 yield int(full)
 
         def count_permutations(counter, n):
-            total = factorial(n)
+            total = fact[n]
             for c in counter.values():
-                total //= factorial(c)
+                total //= fact[c]
 
             if '0' in counter:
                 if counter['0'] == n:
                     return 0
                 counter['0'] -= 1
-                sub_total = factorial(n - 1)
+                sub_total = fact[n - 1]
                 for c in counter.values():
-                    sub_total //= factorial(c)
+                    sub_total //= fact[c]
                 counter['0'] += 1
                 total -= sub_total
             return total
